66a4637ee339eac791defdae00e4e01b369c5bf6
muffin25 <coolgirl_mz@mail.bg>
2018-12-29 13:49:03 +0200

    base of server

---
+++ com/fmi/mpr/hw/chat/src/Server.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Server extends JFrame
+{
+	
+private JTextField usertext;
+private JTextArea chatwindow;
+private ObjectOutputStream output;
+private ObjectInputStream input;
+private ServerSocket server;
+private Socket connection;
+}
