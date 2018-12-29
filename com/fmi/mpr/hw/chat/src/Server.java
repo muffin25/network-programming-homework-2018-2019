b52b58f5eb78647d5e7779722b4de713b037b817
muffin25 <coolgirl_mz@mail.bg>
2018-12-29 14:03:55 +0200

    constructing the GUI of the server

--- com/fmi/mpr/hw/chat/src/Server.java
+++ com/fmi/mpr/hw/chat/src/Server.java
@@ -14,4 +14,23 @@ private ObjectOutputStream output;
 private ObjectInputStream input;
 private ServerSocket server;
 private Socket connection;
+
+// constructor
+public Server()
+{
+	super("Message server");
+	usertext = new JTextField();
+	usertext.setEditable(false);
+	usertext.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			sendmessage(event.getActionCommand());
+			usertext.setText("");
+		}
+	});
+	add(usertext, BorderLayout.NORTH);
+	chatwindow = new JTextArea();
+	add(new JScrollPane(chatwindow));
+	setSize(450,200);
+	setVisible(true);
+}
 }
